6cc8dafbe3bead8e2e91c490788a5e691d1a3ea1
AZIZXlaouiti <azizrocke@gmail.com>
2021-06-07 13:57:23 -0400

    commit tree 1bf4b9f089f5642271b4dabb722f6fd6839edcb1

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Aziz";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
